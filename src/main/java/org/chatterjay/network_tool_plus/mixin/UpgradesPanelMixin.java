a78062e70e4202966cceced0c92be6f6521b7138
Chatterjay <320718257@qq.com>
2026-06-19 15:52:38 +0800

    更新 NeoForge 21.1.219 + AE2 19.2.17 + UpgradesPanel 贴图替换

--- src/main/java/org/chatterjay/network_tool_plus/mixin/UpgradesPanelMixin.java
+++ src/main/java/org/chatterjay/network_tool_plus/mixin/UpgradesPanelMixin.java
@@ -2,7 +2,8 @@ package org.chatterjay.network_tool_plus.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import appeng.client.gui.style.Blitter;
 import appeng.client.gui.widgets.UpgradesPanel;
@@ -12,8 +13,20 @@ import net.minecraft.resources.ResourceLocation;
 @Mixin(UpgradesPanel.class)
 public abstract class UpgradesPanelMixin {
 
-    @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lappeng/client/gui/style/Blitter;texture(Ljava/lang/String;II)Lappeng/client/gui/style/Blitter;"), remap = false)
-    private static Blitter networkToolPlus$redirectTexture(String path, int w, int h) {
-        return Blitter.texture(ResourceLocation.parse("network_tool_plus:textures/guis/extra_panels.png"), 160, 160);
+    @Inject(method = "<clinit>", at = @At("TAIL"), remap = false)
+    private static void networkToolPlus$patchBackground(CallbackInfo ci) {
+        try {
+            var bgField = UpgradesPanel.class.getDeclaredField("BACKGROUND");
+            bgField.setAccessible(true);
+            bgField.set(null, Blitter.texture(
+                ResourceLocation.parse("network_tool_plus:textures/guis/extra_panels.png"), 160, 160));
+
+            var icField = UpgradesPanel.class.getDeclaredField("INNER_CORNER");
+            icField.setAccessible(true);
+            icField.set(null, Blitter.texture(
+                ResourceLocation.parse("network_tool_plus:textures/guis/extra_panels.png"), 160, 160)
+                .src(12, 33, 18, 18));
+        } catch (Exception e) {
+        }
     }
 }
