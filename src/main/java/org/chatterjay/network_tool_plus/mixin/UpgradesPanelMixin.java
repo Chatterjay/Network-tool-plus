183427c70c2dd6d73ced979ee993a0e64b39ef1a
Chatterjay <320718257@qq.com>
2026-06-19 15:29:28 +0800

    fix: UpgradesPanel 贴图路径改为自定义 160x160

--- src/main/java/org/chatterjay/network_tool_plus/mixin/UpgradesPanelMixin.java
+++ src/main/java/org/chatterjay/network_tool_plus/mixin/UpgradesPanelMixin.java
@@ -2,16 +2,18 @@ package org.chatterjay.network_tool_plus.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
+import appeng.client.gui.style.Blitter;
 import appeng.client.gui.widgets.UpgradesPanel;
+
+import net.minecraft.resources.ResourceLocation;
 
 @Mixin(UpgradesPanel.class)
 public abstract class UpgradesPanelMixin {
 
-    @ModifyConstant(method = "<clinit>", constant = @Constant(intValue = 128), remap = false)
-    private static int networkToolPlus$modifyTextureSize(int constant) {
-        return 160;
+    @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lappeng/client/gui/style/Blitter;texture(Ljava/lang/String;II)Lappeng/client/gui/style/Blitter;"), remap = false)
+    private static Blitter networkToolPlus$redirectTexture(String path, int w, int h) {
+        return Blitter.texture(ResourceLocation.parse("network_tool_plus:textures/guis/extra_panels.png"), 160, 160);
     }
 }
